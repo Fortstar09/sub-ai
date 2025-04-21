05a906a2311909de94b007cd024087aec8d2fddf
Fortstar09 <ogunyafort@gmail.com>
2025-04-21 11:18:33 +0100

    implement auth form interface

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,51 +1,12 @@
 import React from "react";
 import Image from "next/image";
 
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen">
-      <section className="hidden w-1/2   items-center bg-[#1BB425]/60 justify-center px-1 py-6 lg:flex xl:w-2/5">
-        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-10">
-          <Image
-            src="/logo/logo-full-white.svg"
-            alt="logo"
-            width={200}
-            height={82}
-            className="h-auto"
-          />
-
-          <div className="space-y-5 text-white">
-            <h1 className="h1">Seamless access to smart recipe substitutions</h1>
-            <p className="body-1">
-            Effortlessly find the best ingredient replacements for any recipe with AI-powered precision.
-            </p>
-          </div>
-          <Image
-            src="/illus/recipe.png"
-            alt="Files"
-            width={300}
-            height={300}
-            className="transition-all hover:rotate-10 hover:scale-105"
-          />
-        </div>
-      </section>
-
-      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
-        <div className="mb-16 lg:hidden">
-          <Image
-            src="/logo/logo-full.svg"
-            alt="logo"
-            width={224}
-            height={82}
-            className="h-auto w-[200px] lg:w-[250px]"
-          />
-        </div>
-
-        {children}
-      </section>
-     
-    </div>
+    <section className=" flex flex-col items-center justify-center gap-2 md:gap-4 min-h-screen px-4 py-10 bg-white">
+      <Image src='/logo/logo-full.svg' alt="logo" height={10} width={100} />
+      {children}
+    </section>
   );
 };
 
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,67 +1,138 @@
-"use client"
+"use client";
 import React from "react";
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
- 
+import Link from "next/link";
+
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  fullName: z.string().min(2).max(50),
 });
 
-
 const AuthForm = ({ type }: { type: string }) => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+    },
+  });
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
-    })
-   
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      toast("Event has been created.")
-      console.log(values)
-    }
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    toast("Event has been created.");
+    console.log(values);
+  }
 
   return (
-    <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              {type}
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
-  </Form>
-
+    <div className=" flex flex-col gap-4 w-full max-w-sm">
+      <div className="flex flex-col mb-8">
+        <h1 className="text-3xl md:text-4xl font-medium text-center">
+          {type === "sign-in" ? "Welcome back" : "Create an account "}
+        </h1>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          {type === "sign-up" && (
+            <>
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Fullname</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="h-13 placeholder:text-[#98a2b3cc] blink text-base font-normal
+    text-[#475367]"
+                        placeholder="Enter your full name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="h-13 placeholder:text-[#98a2b3cc] blink text-base font-normal
+            text-[#475367]"
+                        placeholder="Enter your full name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </>
+          )}
+          
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="h-13 placeholder:text-[#98a2b3cc] blink text-base font-normal
+            text-[#475367]"
+                    placeholder="Enter your full name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="h-13 placeholder:text-[#98a2b3cc] blink text-base font-normal
+            text-[#475367]"
+                    placeholder="Enter your full name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="h-13 w-full bg-emerald-600 hover:bg-emerald-500"
+          >
+            {type === "sign-in" ? "Sign In" : "Create Account"}
+          </Button>
+        </form>
+      </Form>
+      <div className="text-center text-base text-light ">{type === "sign-in" ? "Don't have an account? " : "Already have an account"}{" "}<Link className="text-emerald-600 hover:underline" href={type === 'sign-in'? '/sign-up' : '/sign-in'}>{type === 'sign-in'? 'Sign Up' : 'Sign In'}</Link></div>
+    </div>
   );
 };
 
